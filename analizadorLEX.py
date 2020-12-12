703848e85d2bdb57494efd1c0c0442d345f024f5
R4xm0x99 <51728671+R4xm0x99@users.noreply.github.com>
2020-12-12 16:11:43 -0500

    Añadido de tokens y de funcion para palabras

--- analizadorLEX.py
+++ analizadorLEX.py
@@ -4,86 +4,100 @@ import codecs
 import os
 import sys
 
-reservas = ['BEGIN','END','IF','THEN','WHILE','DO','CALL','CONST','VAR','PROCEDURE','OUT','IN','ELSE']
-tokens = reservas+['ID','NUMBER','PLUS','MINUS','TIMES','DIVIDE','ODD','ASSIGN','NE','LT','LTE','GT','GTE'
-			,'LPARENT','RPARENT','COMMA','SEMMICOLOM','DOT','UPDATE']
 
-t_ignore= '\t'
-t_PLUS= r'\+'
-t_MINUS= r'\-'
-t_TIMES= r'\*'
-t_DIVIDE= r'/'
-t_ODD= r'ODD'
-t_ASSIGN= r'='
-t_NE= r'<>'
-t_LT= r'<'
-t_LTE= r'<='
-t_GT= r'>'
-t_GTE= r'>='
-t_LPARENT= r'\('
-t_RPARENT= r'\)'
-t_COMMA= r','
-t_SEMMICOLOM= r';'
-t_DOT= r'\.'
-t_UPDATE= r':='
+reservas = ['BEGIN', 'END', 'IF', 'THEN', 'WHILE', 'DO', 'CALL', 'CONST', 'VAR', 'PROCEDURE', 'OUT', 'IN', 'ELSE']
 
-def t_ID(t):
-	r'[a-zA-Z_][a-zA-Z0_9_]*'
-	if t.value.upper() in reservadas:
-		t.value=t.value.upper()
-		t.type=t.value
-	return t
-	
+tokens = reservas+['ID', 'NUMBER','PLUS','MINUS','TIMES','DIVIDE','ODD','ASSIGN','NE','LT','LTE','GT','GTE'
+			,'LPARENT','RPARENT','COMMA','SEMMICOLOM','DOT','UPDATE', 'SPACE', 'PALABRA', 'RCORT', 'LCORT','ARROBA',
+                   'COLOM', 'PREGUNTA', 'PERCENT', 'DOLAR']
+
+t_ignore = '\t'
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_ODD = r'ODD'
+t_ASSIGN = r'='
+t_NE = r'<>'
+t_LT = r'<'
+t_LTE = r'<='
+t_GT = r'>'
+t_GTE = r'>='
+t_LPARENT = r'\('
+t_RPARENT = r'\)'
+t_COMMA = r', '
+t_SEMMICOLOM = r';'
+t_DOT = r'\.'
+t_UPDATE = r':='
+t_SPACE  = r'\ '
+t_RCORT = r'\['
+t_LCORT = r'\]'
+t_ARROBA = r'\@'
+t_COLOM = r'\:'
+t_PREGUNTA = r'\?'
+t_PERCENT = r'\%'
+t_DOLAR = r'\$'
+
+def t_PALABRA(t):
+    r'[a-zA-Z_][a-zA-Z_]*'
+    if t.value.upper() in reservas:
+        t.value = t.value.upper()
+        t.type = t.value
+    return t
+
 def t_newline(t):
-	r'\n+'
-	t.lexer.lineno += len(t.value)
-	
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
 def COMMENT(t):
-	r'\#.*'
-	
+    r'\#.*'
+
 
 def t_NUMBER(t):
-	r'\d+'
-	t.value = int(t.value)
-	return t
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
 
 def t_error(t):
-	print "caracter ilegal '%s'" % t.value[0]
-	t.lexer.skip(1)
+    print("caracter ilegal '%s'" % t.value[0])
+    t.lexer.skip(1)
+
 
 def buscarFicheros(directorio):
-	ficheros = []
-	numArchivo = ''
-	respuesta = false
-	cont= 1
+    ficheros = []
+    numArchivo = ''
+    respuesta = False
+    cont = 1
 
-	for base,dirs,files in os.walk(directorio):
-		ficheros.append(files)
+    for base, dirs, files in os.walk(directorio):
+        ficheros.append(files)
 
-	for file in files:
-		print str(cont)+". "+file
-		cont = cont+1
+    for file in files:
+        print(str(cont) + ". " + file)
+        cont = cont + 1
 
-	while respuesta == False :
-		numArchivo= raw_input('\nNumero del test: ')
-		for file in files:
-			if file== files[int(numArchivo)-1]:
-				respuesta= True
-				break
-	print "Has escogido \"%s\" \n" %files[int(numArchivo)-1]
+    while respuesta == False:
+        numArchivo = input('\n Numero del test: ')
+        for file in files:
+            if file == files[int(numArchivo) - 1]:
+                respuesta = True
+                break
+    print("Usted ha seleccionado el archivo:  \"%s\" \n" % files[int(numArchivo) - 1])
+    return files[int(numArchivo) - 1]
 
-	return files[int(numArchivo)-1]
 
-directorio = 'C:/Users/Martin/Desktop/Compilador_PL0-master/analizador version l/test/'
+directorio = 'C:/Users/Alejandro De Puy/Documents/ProyectodeLengajes/analizadorversion3/test/'
 archivo = buscarFicheros(directorio)
-test = directorio+archivo
-fp = codecs.open(test,"r","utf-8")
+test = directorio + archivo
+fp = codecs.open(test, "r", "utf-8")
 cadena = fp.read()
 fp.close()
 
 analizador = lex.lex()
-analizadorinput(cadena)
+analizador.input(cadena)
 while True:
-	tok = analizador.token()
-	if not tok : break
-	print tok
+    tok = analizador.token()
+    if not tok: break
+    print(tok)
